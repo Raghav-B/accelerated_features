c62d533c872bf70cdc7fd8e747c1d18693bf0764
Raghav-B <raghavbhardwaj1998@gmail.com>
2025-04-18 13:04:46 +0800

    fix xfeat.py to work with lightglue notebook

--- modules/xfeat.py
+++ modules/xfeat.py
@@ -239,8 +239,8 @@ class XFeat(nn.Module):
 			self.lighterglue = LighterGlue()
 
 		data = {
-				'keypoints0': d0['keypoints'][None, ...],
-				'keypoints1': d1['keypoints'][None, ...],
+				'keypoints0': d0['keypoints'][None, ...][:, :, :2],
+				'keypoints1': d1['keypoints'][None, ...][:, :, :2],
 				'descriptors0': d0['descriptors'][None, ...],
 				'descriptors1': d1['descriptors'][None, ...],
 				'image_size0': torch.tensor(d0['image_size']).to(self.dev)[None, ...],
